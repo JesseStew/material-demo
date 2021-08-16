600c960e45436b1349547a409d821d72bca5cb7f
Jesse Stewart <jessestewart@BVL-WX743293714.localdomain>
2021-08-16 13:53:40 -0500

    Icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { MaterialModule } from './material/material.module';
 import { TypographyComponent } from './typography/typography.component';
 import { ButtonComponent } from './button/button.component';
 import { ButtonToggleComponent } from './button-toggle/button-toggle.component';
+import { IconComponent } from './icon/icon.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TypographyComponent,
     ButtonComponent,
-    ButtonToggleComponent
+    ButtonToggleComponent,
+    IconComponent
   ],
   imports: [
     BrowserModule,
