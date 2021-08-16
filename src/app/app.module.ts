5305e36f3aa5436f9857636e916b09f4f7894fec
Jesse Stewart <jessestewart@BVL-WX743293714.localdomain>
2021-08-16 13:19:49 -0500

    Added Material Button Variations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { TypographyComponent } from './typography/typography.component';
+import { ButtonComponent } from './button/button.component';
+import { ButtonToggleComponent } from './button-toggle/button-toggle.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TypographyComponent
+    TypographyComponent,
+    ButtonComponent,
+    ButtonToggleComponent
   ],
   imports: [
     BrowserModule,
